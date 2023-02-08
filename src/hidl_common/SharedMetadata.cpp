e89318fc24fc8ac76db7ee15b54fb3766ec7addf
Zhen Chen <chenzhen@rock-chips.com>
2023-02-08 16:23:16 +0800

    Android 13.0: set_smpte2094_40(): resolve error of "E gralloc4: Empty SMPTE 2094-40 data"
    参照 3588_13.0 中 g15(r40) gralloc 中的实现来修改.
    
    Change-Id: I7d21c38e7fc2dbc38051fc37ca756904a5b827d2

--- src/hidl_common/SharedMetadata.cpp
+++ src/hidl_common/SharedMetadata.cpp
@@ -256,15 +256,20 @@ void get_smpte2094_40(const private_handle_t *hnd, std::optional<std::vector<uin
 
 android::status_t set_smpte2094_40(const private_handle_t *hnd, const std::optional<std::vector<uint8_t>> &smpte2094_40)
 {
-	if (!smpte2094_40.has_value() || smpte2094_40->size() == 0)
+	auto *metadata = reinterpret_cast<shared_metadata *>(hnd->attr_base);
+	if (!smpte2094_40.has_value())
 	{
-		MALI_GRALLOC_LOGE("Empty SMPTE 2094-40 data");
-		return android::BAD_VALUE;
+		metadata->smpte2094_40.size = 0;
+		return android::OK;
 	}
 
-	auto *metadata = reinterpret_cast<shared_metadata *>(hnd->attr_base);
-	const size_t size = smpte2094_40.has_value() ? smpte2094_40->size() : 0;
-	if (size > metadata->smpte2094_40.capacity())
+	const size_t size = smpte2094_40->size();
+	if (size == 0)
+	{
+		MALI_GRALLOC_LOGE("SMPTE 2094-40 vector is empty");
+		return android::BAD_VALUE;
+	}
+	else if (size > metadata->smpte2094_40.capacity())
 	{
 		MALI_GRALLOC_LOGE("SMPTE 2094-40 metadata too large to fit in shared metadata region");
 		return android::BAD_VALUE;
